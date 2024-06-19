f8797d7c10e2922f6f125d68c507f9de5808e291
santilaguado <santiagolaguado57@gmail.com>
2024-06-19 17:08:56 -0500

    v1.0.0

---
+++ juego_ahorcado/src/main/java/com/ahorcado/Main.java
@@ -0,0 +1,62 @@
+package com.ahorcado;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+        ArrayList<String> palabras = new ArrayList<String>(Arrays.asList("codigo", "compilador", "arreglo", "javascript", "terminal", "metodo", "funcion", "software", "sistema", "datos"));
+    
+        Random palrand = new Random();
+        int index = palrand.nextInt(palabras.size());
+        String palselecc = palabras.get(index);
+        StringBuilder progreso = new StringBuilder("_ ".repeat(palselecc.length()));
+
+        ArrayList<Character> letrasAdivinadas = new ArrayList<>();
+        int intentos = palselecc.length();
+        Scanner scanner = new Scanner(System.in);
+
+        while (intentos > 0 && progreso.toString().contains("_")) {
+            System.out.println(" ");
+            System.out.println("Tienes " + intentos + " intentos");
+            System.out.println("Adivina la palabra: " + progreso);
+            System.out.println(palselecc);
+            System.out.print("Introduce una letra: ");
+            char letra = scanner.next().charAt(0);
+
+            if (letrasAdivinadas.contains(letra)) {
+                System.out.println(" ");
+                System.out.println("*Ya has adivinado esa letra. Intenta de nuevo*");
+            } else if (palselecc.contains(String.valueOf(letra))) {
+                letrasAdivinadas.add(letra);
+
+                for (int i = 0; i < palselecc.length(); i++) {
+                    if (letra == palselecc.charAt(i)) {
+                        progreso.setCharAt(i*2, letra);
+                    }
+                }
+                
+            } else {
+                intentos--;
+            }
+        }
+
+        String palabrafinal = progreso.toString().replace(" ", "");
+        
+        if (palabrafinal.equals(palselecc)) {
+            System.out.println(" ");
+            System.out.println("FELICITACIONES! Has adivinado la palabra!");
+        } else {
+            System.out.println(" ");
+            System.out.println("Has perdido! La palabra era: " + palselecc);
+        }
+
+    }
+
+
+}
